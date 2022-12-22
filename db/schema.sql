d7381b9db275e96cd66eb2e10903430d00c79f4d
sizeru <gaafts@gmail.com>
2022-12-22 10:38:18 -0600

    Add database schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+-- This is used mainly for reference. The database is already configured
+
+CREATE TABLE IF NOT EXISTS pdfs (
+    id              SERIAL PRIMARY KEY,
+    pdf_type        INTEGER NOT NULL,
+    pdf_date        TIMESTAMP WITH TIME ZONE,
+    customer        VARCHAR ( 50 ),
+    relative_path   VARCHAR ( 60 )
+    -- other vars come here eventually
+);
